1ddd9bcc4f8b2a5ebbd4a8550d2d0decf0574764
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2022-03-02 10:32:37 +0100

    editing readMe, working on templates, writing views for dashboard app, ...

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,23 +9,29 @@ urlpatterns = [
     # 
     path('', views.dashboard, name='dashboard'),
     
+    #
+    # path('admin/home', views.dashboard_admin, name='dashboard_admin'),
     
-    # path('add_card', admin_views.add_card, name="add_card"),
+    #
+    # path('admin/home', views.dashboard_admin, name='dashboard_admin'),
     
-    # path('manage_card', admin_views.manage_card, name="manage_card"),
     
-    # path('manage_marchand', admin_views.manage_marchand, name="manage_marchand"),
+    # path('add/card', views.add_card, name="add_card"),
     
-    # path('manage_transaction', admin_views.manage_transaction, name="manage_transaction"),
+    # path('manage/card', views.manage_card, name="manage_card"),
     
-    # path('top_up_card', admin_views.top_up_card, name="top_up_card"),
+    # path('manage/users', views.manage_users, name="manage_users"),
+    
+    # path('manage/transaction', views.manage_transaction, name="manage_transaction"),
+    
+    # path('topup/card', views.top_up_card, name="top_up_card"),
 
 
     # path('contact', views.contact, name="contact"),
 
     # path('check_gift_card_exist', views.check_gift_card_exist, name="check_gift_card_exist"),
     
-    # path('debiter', views.debit_card, name="debit_card"),
+    # path('debit/card', views.debit_card, name="debit_card"),
 
     # path('transaction', views.transaction, name="transaction"),
     

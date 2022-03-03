2e53e9d5be0a632515a9d5d747d9316f365cef1f
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2022-03-03 10:28:46 +0100

    editing views, ...

--- dashboard/views.py
+++ dashboard/views.py
@@ -103,7 +103,7 @@ def debit_card(request):
                 if debit_amount > card_amount:
                     status = "Failed: Fond Insuffisant"
                     
-                    messages.warning(request, "Failed: Fond Insuffisant")
+                    messages.error(request, "Failed: Fond Insuffisant")
                 else:
                     status = "Succeeded"
                     card_amount = GiftCard.objects.get(code=card_code).amount
@@ -140,61 +140,6 @@ def debit_card(request):
         
         return render(request, "dashboard/debit_card.html", context)
 
-    
-    #     return redirect('dashboard')
-
-    #     elif user_allowed.account_type == "1":
-
-    #         merchant = User.objects.get(id=user_allowed.id)
-
-    #         if request.method == 'POST':
-    #             card_code = request.POST.get("card_code")
-    #             debit_amount = float(request.POST.get("debit_amount"))
-
-    #             gift_card = GiftCard.objects.get(code=card_code)
-    #             card_amount = gift_card.amount
-
-    #             try:
-
-    #                 if debit_amount > card_amount:
-    #                     status = "Failed: Fond Insuffisant"
-                        
-    #                     messages.warning(request, "Failed: Fond Insuffisant")
-    #                 else:
-    #                     status = "Succeeded"
-    #                     card_amount = GiftCard.objects.get(code=card_code).amount
-    #                     card_amount = card_amount - debit_amount
-    #                     GiftCard.objects.filter(code=card_code).update(amount=card_amount)
-                        
-    #                     messages.success(request, "Transaction Completed")
-
-    #                 debit = Debit(
-    #                     card_id=gift_card,
-    #                     user=merchant,
-    #                     debit_amount=debit_amount,
-    #                     status=status
-    #                 )
-    #                 debit.save()
-                    
-
-    #                 return redirect(reverse("debit_card"))
-    #             except Exception as e:
-                    
-    #                 print("\n")
-    #                 print("******************** Exception gotten in debit card ********************")
-    #                 print(e)
-                    
-    #                 messages.error(request, "Failed to Complete Transaction")
-
-    #                 return redirect(reverse("main:debit_card"))
-
-    #         return render(request, "main/marchand/debit_card.html",
-    #                       {
-    #                           "user": merchant,
-    #                       }
-    #                       )
-    
-    # return redirect("dashboard")
 
 
 
@@ -203,11 +148,11 @@ def transaction(request):
     user_allowed = request.user
 
     if user_allowed.is_authenticated:
-        if user_allowed.account_type == "0":
+        if user_allowed.account_type == "0" or user_allowed.is_superuser:
 
             # transaction
 
-            debit = Debit.objects.all().filter(user=merchant)
+            debit = Debit.objects.all()
             
             context = {
                 "debit": debit,
@@ -218,12 +163,10 @@ def transaction(request):
 
         elif user_allowed.account_type == "1":
 
-            merchant = User.objects.get(id=user_allowed.id)
-
-            debit = Debit.objects.all().filter(user=merchant)
+            debit = Debit.objects.all().filter(user=user_allowed)
             
             context = {
-                "user": merchant,
+                "user": user_allowed,
                 "debit": debit,
             }
 
@@ -233,3 +176,124 @@ def transaction(request):
     return redirect("dashboard")
         
 
+
+def add_card(request):
+    user_allowed = request.user
+
+    if user_allowed.is_authenticated:
+
+        if user_allowed.user_type == "1":
+
+            if request.method == 'POST':
+
+                card_amount = request.POST.get("card_amount")
+                card_amount = float(card_amount)
+
+                try:
+                    card_count = GiftCard.objects.all().count()
+
+                    x = date.today()
+                    x = (str(x)).replace("-", "")
+
+                    if card_count == 9:
+                        card_code = "CK" + x + "00" + str(card_count+1)
+                    elif (card_count / 10) < 1:
+                        card_code = "CK" + x + "000" + str(card_count+1)
+                    elif ((card_count + 1) / 10) < 10:
+                        card_code = "CK" + x + "00" + str(card_count+1)
+                    else:
+                        card_code = "CK" + x + "0" + str(card_count+1)
+
+                    card_model = GiftCard(amount=card_amount, code=card_code)
+                    card_model.save()
+                    messages.success(request, "Successfully Added Gift Card")
+
+                    return redirect(reverse("main:add_card"))
+                except Exception as e:
+                    print(e)
+                    messages.error(request, "Failed To Add Gift Card")
+
+                    return redirect(reverse("main:add_card"))
+
+            return render(request, "main/admin/add_card.html")
+
+        elif user_allowed.user_type == "2":
+
+            return redirect('main:marchand_home')
+
+        else:
+
+            return redirect('main:index')
+    else:
+        return redirect('main:admin_login')
+
+
+def manage_card(request):
+    user = request.user
+
+    if user.is_authenticated:
+        if user.user_type == "1":
+
+            gift_card = GiftCard.objects.all()
+            gift_card_count = gift_card.count()
+
+            return render(request, "main/admin/manage_card.html",
+                          {
+                              "gift_card": gift_card,
+                              "gift_card_count": gift_card_count,
+                          }
+                          )
+
+        elif user.user_type == "2":
+            return redirect('main:marchand_home')
+
+        else:
+            return redirect('main:index')
+    else:
+        return render(request, "main/admin/index.html")
+
+
+
+#
+@login_required(redirect_field_name='login')
+def top_up_card(request):
+
+    user_allowed = request.user
+
+    if user_allowed.is_authenticated:
+        
+        if user_allowed.account_type == "0" or user_allowed.is_superuser:
+
+            if request.method == 'POST':
+                card_id = request.POST.get("card")
+                top_amount = request.POST.get("top_amount")
+                top_amount = float(top_amount)
+
+                gift_card = GiftCard.objects.get(id=card_id)
+                card_amount = gift_card.amount
+
+                try:
+
+                    card_amount = card_amount + top_amount
+
+                    GiftCard.objects.filter(id=card_id).update(amount=card_amount)
+
+                    messages.success(request, "Transaction Completed")
+
+                    return redirect(reverse("main:top_up_card"))
+                except:
+                    messages.error(request, "Failed to Complete Transaction.")
+
+                    return redirect(reverse("main:top_up_card"))
+
+            context = {
+                'gift_card': GiftCard.objects.all(),
+                'user': user_allowed,
+            }
+            
+            return render(request, "main/admin/top_up_card.html", context)
+
+
+    return redirect("dashboard")
+    
+    
